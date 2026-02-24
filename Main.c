ec3e81316944cc2b6cdbe8f268394bf319274579
spiderasasen <spiderasasen@gmail.com>
2026-02-24 09:01:52 -0500

    made the random number gen

--- Main.c
+++ Main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+
+
+//getting a random number from 1 - 25
+int get_number() {
+    srand(time(NULL));
+    int number = (rand() % 25) + 1;
+    return number;
+}
+
+
+// main
 int main() {
-    printf("Hello world");
+    printf("Hello world\n");
+    int number = get_number();
+    printf("%d",number);
     return 0;
 }
